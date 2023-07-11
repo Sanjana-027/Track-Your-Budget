cd41b04feccb02b868690fc69c1009959cb41a63
Sanjana-027 <sanjanasingh02710@gmail.com>
2023-07-11 22:50:47 +0530

    components created

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { GoalDetailsComponent } from './goal-details/goal-details.component';
+import { AddGoalsComponent } from './add-goals/add-goals.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    GoalDetailsComponent,
+    AddGoalsComponent
   ],
   imports: [
     BrowserModule
