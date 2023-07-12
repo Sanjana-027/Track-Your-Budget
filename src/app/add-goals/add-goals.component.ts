13dca4a4c3bc5a767e84d97bf88af3d6249ee525
Sanjana-027 <sanjanasingh02710@gmail.com>
2023-07-12 23:42:55 +0530

    [add-goal]

--- src/app/add-goals/add-goals.component.ts
+++ src/app/add-goals/add-goals.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { AppService } from '../app.sevice';
+
 
 @Component({
   selector: 'app-add-goals',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./add-goals.component.scss']
 })
 export class AddGoalsComponent {
+
+  goalTypes : any;
+
+  constructor(private appService: AppService) {}
+
+  ngOnInit() {
+    this.goalTypes = this.appService.goalTypes;
+  }
 
 }
